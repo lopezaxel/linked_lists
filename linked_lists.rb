1d4064a72afbae36d2f5fe8fc4bebdfbf96f5605
Axel Lopez <lopezaxel@protonmail.com>
2020-07-01 08:54:29 -0300

    Add append, prepend methods

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,13 +1,29 @@
 class LinkedList
+  attr_accessor :list
 
+  def initialize()
+    @list = []
+  end
+
+  def append(value)
+    list << Node.new(value)
+  end
+
+  def prepend(value)
+    list.unshift(Node.new(value))
+  end
 end
 
 class Node
-  def value
-    nil
-  end
+  attr_accessor :value, :next_node
 
-  def next_node
-    nil
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
   end
 end
+
+list_linked = LinkedList.new
+list_linked.append(5)
+list_linked.prepend(1)
+p list_linked
